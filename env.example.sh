d84346259d64a346bb293bc03714675b6d9da50e
Heston Liebowitz <heston@matter.com>
2018-02-18 16:25:22 -0800

    Added example environment file

---
+++ env.example.sh
@@ -0,0 +1,14 @@
+########################################
+##          System Settings           ##
+########################################
+
+##
+## Firebase (required)
+##
+## These settings configure how the client communicates with the server.
+## The values will be specific to your Firebase server deployment.
+##
+
+# export LC_FIREBASE_APP_NAME=label-commander
+# export LC_FIREBASE_KEY_PATH=/home/pi/.firebasekey
+# export LC_FIREBASE_API_KEY=key

ee78f47338aa9b2d1e40c33484b7364c5044edf8
Heston Liebowitz <heston@matter.com>
2018-03-15 19:47:35 -0700

    Actually print the label.

--- labelcommander/main.py
+++ labelcommander/main.py
@@ -10,8 +10,8 @@ def print_label(text):
     try:
         tex_path = render.generate(text)
         pdf_path = output.pdftex(tex_path)
-        logger.info(pdf_path)
-        # output.print(pdf_path)
+        logger.debug('Printing PDF file: %s', pdf_path)
+        output.print(pdf_path)
     except output.PrintError as e:
         logger.error('Could not print label: %s', e)
         return False

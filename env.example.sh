cdf6714c22b592743241702baa14953f652e1898
Heston Liebowitz <heston@junelife.com>
2022-05-04 21:20:40 -0600

    Many updates

--- env.example.sh
+++ env.example.sh
@@ -9,17 +9,18 @@
 ## The values will be specific to your Firebase server deployment.
 ##
 
-# export LC_FIREBASE_APP_NAME=label-commander
-# export LC_FIREBASE_KEY_PATH=/home/pi/.firebasekey
-# export LC_FIREBASE_API_KEY=key
+export LC_FIREBASE_APP_NAME=my-app
+export LC_FIREBASE_KEY_PATH=/home/pi/firebasekey.json
+export LC_FIREBASE_API_KEY=key
+export LC_FIREBASE_DATABASE_URL=https://my-app-name-default-rtdb.firebaseio.com/
 
 
 ## The name of the LabelWriter printer as it appears in CUPS
 
-# export LC_CUPS_PRINTER_NAME=DYMO_LabelWriter_330
+export LC_CUPS_PRINTER_NAME=DYMO_LabelWriter_330
 
 
 ## The desired log level
 ## This should probably be ERROR or INFO during normal usage
 
-# export LC_LOGGING_LEVEL=DEBUG
+export LC_LOGGING_LEVEL=DEBUG
--- labelcommander/settings.py
+++ labelcommander/settings.py
@@ -11,7 +11,10 @@ FIREBASE_KEY_PATH = os.getenv('LC_FIREBASE_KEY_PATH', '/home/pi/.firebasekey')
 # Firebase web API key
 FIREBASE_API_KEY = os.getenv('LC_FIREBASE_API_KEY')
 
-# The path to the print queue collection
+# Firebase Realtime Database URL
+FIREBASE_DATABASE_URL = os.getenv('LC_FIREBASE_DATABASE_URL')
+
+# The path to the print queue collection in the database
 FIREBASE_PRINT_QUEUE_PATH = 'print_jobs'
 
 # The name of the printer in CUPS
